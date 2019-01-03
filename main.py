9484e8e19973065f44c0e25556a59ab63cb67bd1
Artem Shtelzer <shtelzerartem@gmail.com>
2019-01-03 17:20:14 +0300

    WithoutSemaphors

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
-from threading import Thread, Lock, BoundedSemaphore
+from threading import Thread, Lock
 import time
+import random
 import logging
 
 
-global numPhil, timeout, counter, semaphore
+global numPhil, timeout, counter, forks
 
 logger = logging.getLogger('PhilosophsLogger')
 logging.basicConfig(level=logging.DEBUG)
 
 numPhil = 5
-timeout = 7
+timeout = 30
 debug = False
 
 logger.propagate = debug
 counter = [0 for i in range(numPhil)]
-semaphore = BoundedSemaphore(numPhil - 2)
+forks = [Lock() for i in range(numPhil)]
+
 
 
 class Philosopher(Thread):
@@ -29,47 +31,48 @@ class Philosopher(Thread):
 
     # Start eating
     def run(self):
-        semaphore.acquire()
+        if forks[self.left].acquire(timeout=random.randint(1, 10)/100):
+            logger.debug('Философ {} номер {} взял левую вилку'.format(self.name, self.index))
+            time.sleep(random.randint(1, 10)/100)
 
-        self.left.acquire()
-        logger.debug('Философ {} номер {} взял левую вилку'.format(self.name, self.index))
-        time.sleep(0.05)
+            if forks[self.right].acquire(timeout=random.randint(1, 10)/100):
+                logger.debug('Философ {} номер {} взял правую вилку и начал есть'.format(self.name, self.index))
 
-        self.right.acquire()
-        logger.debug('Философ {} номер {} взял правую вилку и начал есть'.format(self.name, self.index))
+                time.sleep(random.randint(1, 10)/10)
+                logger.debug('Философ {} номер {} поел'.format(self.name, self.index))
 
-        time.sleep(1)
-        logger.debug('Философ {} номер {} поел'.format(self.name, self.index))
+                forks[self.left].release()
+                forks[self.right].release()
 
-        self.left.release()
-        self.right.release()
-        semaphore.release()
-        time.sleep(1)
-        counter[self.index - 1] += 1
+                counter[self.index - 1] += 1
+            else:
+                forks[self.left].release()
 
+            time.sleep(random.randint(1, 10)/10)
+        
         self.run()
 
 def start_play(numPhil):
-    
-    forks = [Lock() for i in range(numPhil)]
+    random.seed(507129)
     
     phil = []
     for i in range(1, numPhil+1):
-        phil.append(Philosopher('Phil'+str(i), i, forks[i-2], forks[i-1]))
+        phil.append(Philosopher('Phil'+str(i), i, i-2, i-1))
 
-    for i in phil:
-        i.start()
+    for i in phil: i.start()
         
-    for i in phil:
-        i.join(timeout)
+    for i in phil: i.join(timeout)
     
     
 if __name__ == '__main__':
     try:
+        print('Debug: %s' % str(debug))
         t = Thread(target=start_play, args=(numPhil, ))
         t.daemon = True
         t.start()
         t.join(timeout)
     finally:
         logger.debug('Finished')
+        print('Timeout: %d seconds' % timeout)
+        print('Number of Philosophs: %d' % numPhil)
         print(counter)
